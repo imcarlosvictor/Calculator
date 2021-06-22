e61de46a3ebe7ee855d0be012776cf16a7a9a91d
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-06-22 01:34:26 -0400

    logic completed

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class Window(qtw.QWidget):
         self.setLayout(qtw.QVBoxLayout())
         self.keypad()
 
-        self.temp_inputs = []
         self.temp_equation = []
 
         self.show()
@@ -26,25 +25,25 @@ class Window(qtw.QWidget):
         self.display_field = qtw.QLineEdit()
 
         # Numbers
-        btn_decimal = qtw.QPushButton('.', clicked=lambda: self.num_press('.'))
-        btn_0 = qtw.QPushButton('0', clicked=lambda: self.num_press('0'))
-        btn_1 = qtw.QPushButton('1', clicked=lambda: self.num_press('1'))
-        btn_2 = qtw.QPushButton('2', clicked=lambda: self.num_press('2'))
-        btn_3 = qtw.QPushButton('3', clicked=lambda: self.num_press('3'))
-        btn_4 = qtw.QPushButton('4', clicked=lambda: self.num_press('4'))
-        btn_5 = qtw.QPushButton('5', clicked=lambda: self.num_press('5'))
-        btn_6 = qtw.QPushButton('6', clicked=lambda: self.num_press('6'))
-        btn_7 = qtw.QPushButton('7', clicked=lambda: self.num_press('7'))
-        btn_8 = qtw.QPushButton('8', clicked=lambda: self.num_press('8'))
-        btn_9 = qtw.QPushButton('9', clicked=lambda: self.num_press('9'))
-        btn_open_bracket = qtw.QPushButton('(', clicked=lambda: self.num_press('('))
-        btn_close_bracket = qtw.QPushButton(')', clicked=lambda: self.num_press(')'))
+        btn_decimal = qtw.QPushButton('.', clicked=lambda: self.key_press('.'))
+        btn_0 = qtw.QPushButton('0', clicked=lambda: self.key_press('0'))
+        btn_1 = qtw.QPushButton('1', clicked=lambda: self.key_press('1'))
+        btn_2 = qtw.QPushButton('2', clicked=lambda: self.key_press('2'))
+        btn_3 = qtw.QPushButton('3', clicked=lambda: self.key_press('3'))
+        btn_4 = qtw.QPushButton('4', clicked=lambda: self.key_press('4'))
+        btn_5 = qtw.QPushButton('5', clicked=lambda: self.key_press('5'))
+        btn_6 = qtw.QPushButton('6', clicked=lambda: self.key_press('6'))
+        btn_7 = qtw.QPushButton('7', clicked=lambda: self.key_press('7'))
+        btn_8 = qtw.QPushButton('8', clicked=lambda: self.key_press('8'))
+        btn_9 = qtw.QPushButton('9', clicked=lambda: self.key_press('9'))
+        btn_open_bracket = qtw.QPushButton('(', clicked=lambda: self.key_press('('))
+        btn_close_bracket = qtw.QPushButton(')', clicked=lambda: self.key_press(')'))
 
         # Operators
-        btn_add = qtw.QPushButton('+', clicked=lambda: self.func_press('+'))
-        btn_subtract = qtw.QPushButton('-', clicked=lambda: self.func_press('-'))
-        btn_multiply = qtw.QPushButton('x', clicked=lambda: self.func_press('*'))
-        btn_divide = qtw.QPushButton('÷', clicked=lambda: self.func_press('/'))
+        btn_add = qtw.QPushButton('+', clicked=lambda: self.key_press('+'))
+        btn_subtract = qtw.QPushButton('-', clicked=lambda: self.key_press('-'))
+        btn_multiply = qtw.QPushButton('x', clicked=lambda: self.key_press('*'))
+        btn_divide = qtw.QPushButton('÷', clicked=lambda: self.key_press('/'))
 
         # Functions
         btn_equals = qtw.QPushButton('=', clicked=lambda: self.result())
@@ -79,54 +78,25 @@ class Window(qtw.QWidget):
         self.layout().addWidget(keypad_container)
 
     # Logic -----------------
-    def num_press(self, num):
+    def key_press(self, key):
         """Stores the digits entered and displays it onscreen."""
 
-        self.temp_inputs.append(num)
-        input = ''.join(self.temp_inputs)
+        self.temp_equation.append(key)
+        inputs = ''.join(self.temp_equation)
         # Display inputs
-        self.display_field.setText(input)
-        # ----------
-        print('num_press()')
-        print(self.temp_inputs)
-        print(self.temp_equation)
-
-    def func_press(self, operator):
-        """Creates the equation."""
-
-        # Append all digits entered to the equation list
-        input = ''.join(self.temp_inputs)
-        self.temp_equation.append(input)
-
-        # Append operator to equation
-        self.temp_equation.append(operator)
-        equation = ''.join(self.temp_equation)
-        # Display equation
-        self.display_field.setText(equation)
-        self.temp_inputs = []
-        # ----------
-        print('fun_press()')
-        print(self.temp_inputs)
-        print(self.temp_equation)
+        self.display_field.setText(inputs)
 
     def result(self):
         """Display result of equation"""
 
         equation = ''.join(self.temp_equation)
-        answer = eval(equation)
+        answer = str(eval(equation))
         self.display_field.setText(answer)
-        # Clear all lists
-        self.clear()
 
     def clear(self):
         """Clears elements from both lists"""
-        self.temp_num = []
         self.temp_equation = []
         self.display_field.setText('')
-        # ----------
-        print('clear()')
-        print(self.temp_num)
-        print(self.temp_equation)
 
 
 # Create Application
